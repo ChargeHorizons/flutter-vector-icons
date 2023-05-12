e51b8dde5f0839b0d34295b9a0da7cfcad83be6d
Jeroen Trzaska <jeroen@trzaska.xyz>
2023-05-12 13:31:26 -0600

    removed un-needed files

--- lib/flutter_vector_icons.dart
+++ lib/flutter_vector_icons.dart
@@ -1,13 +1,3 @@
 library flutter_vector_icons;
 
-export 'src/ant_design.dart';
-export 'src/entypo.dart';
-export 'src/evil_icons.dart';
-export 'src/feather.dart';
-export 'src/fontisto.dart';
-export 'src/foundation.dart';
-export 'src/ionicons.dart';
 export 'src/material_community_icons.dart';
-export 'src/octicons.dart';
-export 'src/simple_line_icons.dart';
-export 'src/zocial.dart';
